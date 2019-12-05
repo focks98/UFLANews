1385d767bffb8262b83a0aa2a67cbe1020c6e1d9
rodherc <rodrigo.herculano@estudante.ufla.br>
2019-12-05 17:37:02 -0300

    errei

--- app/src/app/pages/search/search.page.ts
+++ app/src/app/pages/search/search.page.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { PublisherModel } from 'src/app/model/publisher.model';
+import { SearchService } from 'src/app/services/search.service';
+import { UserModel } from 'src/app/model/user.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-search',
+  selector: 'app-news',
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss'],
 })
+
 export class SearchPage implements OnInit {
 
-  constructor() { }
+  listPublishers: PublisherModel[];
 
-  ngOnInit() {
+  constructor(
+    public searchService: SearchService) {}
+
+  async ngOnInit() {
+    this.listPublishers = await this.searchService.getAll();
   }
 
+  async doRefresh(event: any) {
+    try {
+      this.listPublishers = await this.searchService.getAll();
+    } finally {
+      event.target.complete();
+    }
+  }
+
+  async updateListPublishers(event: any) {
+    this.listPublishers = await this.searchService.searchByName(event.target.value);
+  }
+  
 }
+
